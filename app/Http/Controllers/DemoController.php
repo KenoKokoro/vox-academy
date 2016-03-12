08645820d32bec0e4e72cfc65e29331f850155a2
Stefan Brankovikj <s.brankovik@voxteneo.com.mk>
2016-03-12 16:43:01 +0100

    Change DemoController

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -2,24 +2,12 @@
 //
 //namespace Academy\Http\Controllers;
 //
-////use View;
+//use View;
 //
 //class DemoController extends Controller
 //{
 //    public function index()
 //    {
-//        $test = '';
-//        $test = 'demo watch expression';
-//        $test = 'test';
-//        $test = $this->demoRoute();
-//        dd(config('app.debug'));
-//        return $test;
-////        return View::make('demo', compact("user"));
-//    }
-//
-//    public function demoRoute()
-//    {
-//        $value = 'new value';
-//        return $value;
+//        return View::make('demo');
 //    }
 //}
